7e873174487b9ed64619adca699ea6fed345ff0e
Masamune0221 <dev.masamune02210@gmail.com>
2026-06-18 23:22:26 +0900

    2026/6/18 Masamune storybook導入

---
+++ .storybook/preview.tsx
@@ -0,0 +1,26 @@
+import type { Preview } from '@storybook/nextjs-vite';
+import '../app/globals.css';
+import { ThemeProvider } from '../app/components/theme-provider';
+
+const preview: Preview = {
+  decorators: [
+    (Story) => (
+      <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    a11y: {
+      test: 'todo',
+    },
+  },
+};
+
+export default preview;
--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,6 @@ export default function RootLayout({
           attribute="class"
           defaultTheme="system"
           enableSystem
-          disableTransitionOnChange
         >
           <Header />
           {children}
